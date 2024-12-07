68b17c8d8f769a8bc66352f8f85aac9240e56c09
chiouya <chiouyazo@gmail.com>
2024-12-07 16:19:40 +0100

    added rendering

---
+++ AutoTf.TabletOS/StartScreen.cs
@@ -0,0 +1,34 @@
+using System.Drawing;
+using AutoTf.Logging;
+using AutoTf.Renderer;
+using AutoTf.Renderer.Models;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace AutoTf.TabletOS;
+
+public class StartScreen : IDisposable
+{
+	private readonly RenderEngine _render;
+	private readonly Logger _logger = new Logger();
+
+	public StartScreen()
+	{
+		_render = new RenderEngine(_logger, true, 1024, 600, true);
+
+		Mat background =
+			CvInvoke.Imread(
+				"/home/display/AutoTf.TabletOS/AutoTf.TabletOS/bin/Debug/net8.0/Images/TabletOSEmptyBackground.png");
+		CvInvoke.CvtColor(background, background, ColorConversion.Bgr2Bgra);
+		_render.SetBackgroundImage(background);
+		_render.AddText(new Text("Welcome to TabletOS", .4f, FontFace.HersheyScriptSimplex, new Point(512, 300),
+			new MCvScalar(0), 1));
+	}
+
+	public void Dispose()
+	{
+		_render.Dispose();
+		_logger.Dispose();
+	}
+}
