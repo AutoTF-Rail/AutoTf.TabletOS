d982a2d9b21fc818c53ece7653f31b6659ed9d43
chiouya <chiouyazo@gmail.com>
2024-12-20 20:16:08 +0100

    enabled touch module again

--- AutoTf.TabletOS/StartScreen.cs
+++ AutoTf.TabletOS/StartScreen.cs
@@ -17,7 +17,7 @@ public class StartScreen : IDisposable
 		// _render = new RenderEngine(_logger, true, 1024, 600, true, true);
 		try
 		{
-			_render = new RenderBase(_logger, true, new Size(1024, 1024), new Size(1024, 600), false, true);
+			_render = new RenderBase(_logger, true, new Size(1024, 1024), new Size(1024, 600), true, true);
 		
 			_render.ActiveScreen = new LoginScreen(_render, _logger);
 			_render.ActiveToolbar = new TopScreenBar(_render, _logger);
