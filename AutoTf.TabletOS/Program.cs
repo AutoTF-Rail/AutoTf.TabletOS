13c323261004f9f889f7fe2a5af0bb589de9a82f
chiouya <chiouyazo@gmail.com>
2024-12-21 13:34:15 +0100

    Proper disposing

--- AutoTf.TabletOS/Program.cs
+++ AutoTf.TabletOS/Program.cs
@@ -1,12 +1,17 @@
-﻿using AutoTf.TabletOS;
+﻿using AutoTf.Renderer;
+using AutoTf.TabletOS;
 
 internal class Program
 {
+	private static StartScreen _screen;
 	public static void Main(string[] args)
 	{
 		try
 		{
-			StartScreen screen = new StartScreen();
+			AppDomain.CurrentDomain.ProcessExit += (sender, e) => _screen.Dispose(); 
+			AppDomain.CurrentDomain.UnhandledException += (sender, e) => _screen.Dispose(); 
+			
+			_screen = new StartScreen();
 			Thread.Sleep(-1);
 		}
 		catch (Exception e)
--- AutoTf.TabletOS/StartScreen.cs
+++ AutoTf.TabletOS/StartScreen.cs
@@ -6,7 +6,6 @@ using AutoTf.TabletOS.Screens;
 
 namespace AutoTf.TabletOS;
 
-[ScreenIdentifier("StartScreen")]
 public class StartScreen : IDisposable
 {
 	private readonly IRenderBase _render;
@@ -14,7 +13,6 @@ public class StartScreen : IDisposable
 
 	public StartScreen()
 	{
-		// _render = new RenderEngine(_logger, true, 1024, 600, true, true);
 		try
 		{
 			_render = new RenderBase(_logger, true, new Size(1024, 1024), new Size(1024, 600), true, true);
@@ -26,29 +24,7 @@ public class StartScreen : IDisposable
 		{
 			_logger.Log(e.Message);
 		}
-
-		// Mat background =
-		// CvInvoke.Imread(
-		// "/home/display/AutoTf.TabletOS/AutoTf.TabletOS/bin/Debug/net8.0/Images/TabletOSLogin.png");
-		// CvInvoke.CvtColor(background, background, ColorConversion.Bgr2Bgra);
-		// _render.SetBackgroundImage(background);
-		// _render.AddButton(new Button("Shutdown", .6f, FontFace.HersheySimplex, new Point(15, 15), new Size(125, 30),
-		// 	new MCvScalar(0), new MCvScalar(10, 10, 10), true, new MCvScalar(255,255,255), OnClick));
-		// _render.AddButton(new Button("Lighter", .6f, FontFace.HersheySimplex, new Point(15, 50), new Size(125, 30),
-		// 	new MCvScalar(0), new MCvScalar(10, 10, 10), true, new MCvScalar(255,255,255), _render.HigherBrightness));
-		// _render.AddButton(new Button("Darker", .6f, FontFace.HersheySimplex, new Point(15, 85), new Size(125, 30),
-		// 	new MCvScalar(0), new MCvScalar(10, 10, 10), true, new MCvScalar(255,255,255), _render.LowerBrightness));
-		// _render.AddText(new Text("Welcome to TabletOS", .4f, FontFace.HersheyScriptSimplex, new Point(512, 300),
-		// 	new MCvScalar(0), 1));
 	}
-
-	// private void OnClick()
-	// {
-	// 	_render.AddPopup(new Popup(_render, "Guh???", () => _logger.Log("Clicked yes"), () => _logger.Log("Clicked no")));
-	// 	_render.AddText(new Text("BUTTON CLICK", 1f, FontFace.HersheySimplex, new Point(100, 100),
-	// 		new MCvScalar(255, it * 50, 0), 2));
-	// 	it++;
-	// }
 
 	public void Dispose()
 	{
