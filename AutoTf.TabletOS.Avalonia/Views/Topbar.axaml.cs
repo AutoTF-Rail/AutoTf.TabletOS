a5ea55e607b4e54d3518bd76def9988a1d9f8d7d
chiouya <chiouyazo@gmail.com>
2024-12-23 00:13:17 +0100

    First UI throw

---
+++ AutoTf.TabletOS.Avalonia/Views/Topbar.axaml.cs
@@ -0,0 +1,42 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Threading;
+
+namespace AutoTf.TabletOS.Avalonia.Views;
+
+public partial class TopBar : UserControl
+{
+	private DispatcherTimer _timer;
+	
+	public TopBar()
+	{
+		InitializeComponent();
+		Initialize();
+	}
+
+	private void Initialize()
+	{
+		// QuickMenuGrid.IsVisible = false;
+		
+		
+		_timer = new DispatcherTimer
+		{
+			Interval = TimeSpan.FromSeconds(1) 
+		};
+		_timer.Tick += UpdateClock; 
+		_timer.Start(); 
+
+		UpdateClock(null, null);
+	}
+	
+	private void UpdateClock(object? sender, EventArgs e)
+	{
+		Bluber.Text = DateTime.Now.ToString("dd.MM.yy HH:mm:ss");
+	}
+
+	private void ToggleQuickMenu(object? sender, PointerReleasedEventArgs e)
+	{
+		QuickMenuGrid.IsVisible = !QuickMenuGrid.IsVisible;
+	}
+}
