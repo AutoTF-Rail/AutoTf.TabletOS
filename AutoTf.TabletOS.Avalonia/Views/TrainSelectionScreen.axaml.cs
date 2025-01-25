b80817e6ff575ce199046650d1a85e9d8068f95e
chiouya <chiouyazo@gmail.com>
2025-01-25 19:51:03 +0100

    Added bluetooth discovery

--- AutoTf.TabletOS.Avalonia/Views/TrainSelectionScreen.axaml.cs
+++ AutoTf.TabletOS.Avalonia/Views/TrainSelectionScreen.axaml.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoTf.TabletOS.Models;
@@ -31,76 +35,18 @@ public partial class TrainSelectionScreen : UserControl
 
 	private void LoadNearbyTrains()
 	{
-		Dispatcher.UIThread.Invoke(() =>
-		{
-			NearbyTrains.ItemsSource = new List<TrainAd>()
-			{
-				new TrainAd()
-				{
-					TrainName = "Meow",
-					TrainNum = "123-123"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				},
-				new TrainAd()
-				{
-					TrainName = "Wuff",
-					TrainNum = "456-456"
-				}
-			};
-		});
-		Thread.Sleep(2500);
+		string[]? bridges = RunBridgeScan();
+		
 		Dispatcher.UIThread.Invoke(() =>
 		{
 			NearbyLoadingArea.IsVisible = false;
+			if (bridges != null)
+			{
+				NearbyTrains.ItemsSource = bridges.Select(x => new TrainAd()
+				{
+					TrainName = x.Replace("CentralBridge-", "") 
+				}).ToList();
+			}
 		});
 	}
 
@@ -110,5 +56,50 @@ public partial class TrainSelectionScreen : UserControl
 		{
 			// Show text that trains aren't available due to no internet
 		}
+	}
+
+	private void NearbyTrains_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+	{
+		NearbyTrains.SelectedItem = null;
+	}
+
+	private void OtherTrains_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+	{
+		OtherTrains.SelectedItem = null;
+	}
+	
+	public string[]? RunBridgeScan()
+	{
+		ProcessStartInfo processStartInfo = new ProcessStartInfo()
+		{
+			FileName = "hcitool",
+			Arguments = "lescan",
+			RedirectStandardOutput = true,
+			UseShellExecute = false,
+			CreateNoWindow = true
+		};
+
+		Process process = new Process()
+		{
+			StartInfo = processStartInfo
+		};
+
+		process.Start();
+		Thread.Sleep(1500);
+		process.Kill();
+
+		string output = process.StandardOutput.ReadToEnd();
+
+		process.WaitForExit();
+
+		return FilterForBridge(output);
+	}
+	
+	private string[]? FilterForBridge(string output)
+	{
+		Regex regex = new Regex(@"([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2})\s*(CentralBridge-[^\s]+)");
+
+		MatchCollection matches = regex.Matches(output);
+		return matches.Select(x => x.Groups[2].Value).ToArray();
 	}
 }
