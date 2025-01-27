e9ef2c6469c5a76dd6d4fac2e83b4421ebb53f46
chiouya <chiouyazo@gmail.com>
2025-01-27 15:32:03 +0100

    Revert "Fixed yubikey explosion (TODO)"
    This reverts commit dca472e3047a2fff9983728619bc6b96def734fc.

--- AutoTf.TabletOS.Avalonia/Views/MainView.axaml.cs
+++ AutoTf.TabletOS.Avalonia/Views/MainView.axaml.cs
@@ -25,65 +25,56 @@ public partial class MainView : UserControl
 
 	private void ListenForYubikey()
 	{
-		try
+		ProcessStartInfo processStartInfo = new ProcessStartInfo
 		{
+			FileName = "udevadm",
+			Arguments = "monitor --subsystem-match=usb --property",
+			RedirectStandardOutput = true,
+			UseShellExecute = false,
+			CreateNoWindow = true
+		};
 
-			ProcessStartInfo processStartInfo = new ProcessStartInfo
+		Process process = new Process { StartInfo = processStartInfo };
+		process.OutputDataReceived += (sender, e) =>
+		{
+			if (e.Data != null)
 			{
-				FileName = "udevadm",
-				Arguments = "monitor --subsystem-match=usb --property",
-				RedirectStandardOutput = true,
-				UseShellExecute = false,
-				CreateNoWindow = true
-			};
-
-			Process process = new Process { StartInfo = processStartInfo };
-			process.OutputDataReceived += (sender, e) =>
-			{
-				if (e.Data != null)
+				if (e.Data.Contains("Yubico") || e.Data.Contains("YubiKey"))
 				{
-					if (e.Data.Contains("Yubico") || e.Data.Contains("YubiKey"))
-					{
-						Dispatcher.UIThread.Invoke(() =>
-						{	
-							LoadingName.Text = "Getting key..";
-							LoadingArea.IsVisible = true;
-							
-							IYubiKeyDevice? device = YubiKeyDevice.FindAll().FirstOrDefault();
-							if (device == null)
-								return;
+					Dispatcher.UIThread.Invoke(() =>
+					{	
+						LoadingName.Text = "Getting key..";
+						LoadingArea.IsVisible = true;
+						
+						IYubiKeyDevice? device = YubiKeyDevice.FindAll().FirstOrDefault();
+						if (device == null)
+							return;
 
-							using (OathSession session = new OathSession(device))
+						using (OathSession session = new OathSession(device))
+						{
+							foreach (Credential credential in session.GetCredentials())
 							{
-								foreach (Credential credential in session.GetCredentials())
-								{
-									if (credential.Issuer != "AutoTF")
-										return;
-									Statics.YubiCode = session.CalculateCredential(credential).Value!;
-									Statics.YubiSerial = device.SerialNumber!.Value;
-									Statics.YubiTime = DateTime.UtcNow;
-								}
+								if (credential.Issuer != "AutoTF")
+									return;
+								Statics.YubiCode = session.CalculateCredential(credential).Value!;
+								Statics.YubiSerial = device.SerialNumber!.Value;
+								Statics.YubiTime = DateTime.UtcNow;
 							}
-							// TODO: Error handling if no cred was found.
-							if (DataContext is MainWindowViewModel viewModel)
-							{
-								viewModel.ActiveView = new TrainSelectionScreen();
-							}
-							LoadingArea.IsVisible = false;
-						});
-						// TODO: Requires ppa:yubico/stable - yubikey-manager
-					}
+						}
+						// TODO: Error handling if no cred was found.
+						if (DataContext is MainWindowViewModel viewModel)
+						{
+							viewModel.ActiveView = new TrainSelectionScreen();
+						}
+						LoadingArea.IsVisible = false;
+					});
+					// TODO: Requires ppa:yubico/stable - yubikey-manager
 				}
-			};
+			}
+		};
 
-			process.Start();
-			process.BeginOutputReadLine();
-		}
-		catch (Exception e)
-		{
-			Dispatcher.UIThread.Invoke(() => { LoadingArea.IsVisible = false; });
-			ListenForYubikey();
-		}
+		process.Start();
+		process.BeginOutputReadLine();
 	}
 
 	private void BrightnessChanged()
