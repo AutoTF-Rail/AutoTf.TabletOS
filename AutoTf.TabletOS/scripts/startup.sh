da27241ed8aabade7faa582ad64b3b5286a7286e
chiouya <chiouyazo@gmail.com>
2025-01-28 18:54:59 +0100

    Added logger to singletons

--- AutoTf.TabletOS/scripts/startup.sh
+++ AutoTf.TabletOS/scripts/startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Yes, this works. Only this way it works.
 ssh root@127.0.0.1 "cd /home/display/AutoTf.TabletOS/AutoTf.TabletOS.Avalonia && /usr/local/bin/dotnet run --no-build -c RELEASE -m --drm"
