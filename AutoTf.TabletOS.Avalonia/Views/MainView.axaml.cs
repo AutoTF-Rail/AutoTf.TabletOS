d02445dbbe153785b83df6659bd6b2eff7f0cd2e
chiouya <chiouyazo@gmail.com>
2025-01-28 16:23:10 +0100

    test

--- AutoTf.TabletOS.Avalonia/Views/MainView.axaml.cs
+++ AutoTf.TabletOS.Avalonia/Views/MainView.axaml.cs
@@ -16,9 +16,10 @@ public partial class MainView : UserControl
 	public MainView()
 	{
 		InitializeComponent();
-
+		LoadingArea.IsVisible = true;
+		
 		Statics.BrightnessChanged += BrightnessChanged;
-		LoadingArea.IsVisible = false;
+		// LoadingArea.IsVisible = false;
 
 		Task.Run(ListenForYubikey);
 	}
