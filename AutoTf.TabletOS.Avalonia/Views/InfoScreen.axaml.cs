c1505dda1e3906b9ad4fd929fa7ad5ac96931797
chiouya <chiouyazo@gmail.com>
2025-01-30 12:50:40 +0100

    updated update UI

--- AutoTf.TabletOS.Avalonia/Views/InfoScreen.axaml.cs
+++ AutoTf.TabletOS.Avalonia/Views/InfoScreen.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using AutoTf.TabletOS.Avalonia.ViewModels;
 using AutoTf.TabletOS.Models;
 using Avalonia;
@@ -37,52 +38,77 @@ public partial class InfoScreen : UserControl
 		}
 	}
 
-	private void Update_Click(object? sender, RoutedEventArgs e)
+	private async void Update_Click(object? sender, RoutedEventArgs e)
 	{
 		if (!NetworkManager.IsInternetAvailable())
 			return;
 
-		Dispatcher.UIThread.Invoke(() => UpdateText.IsVisible = true);
-
+		Dispatcher.UIThread.InvokeAsync(() => UpdateText.IsVisible = true);
+		await Task.Delay(50);
+		
 		string prevDir = Directory.GetCurrentDirectory();
 		Directory.SetCurrentDirectory("/home/display/AutoTf.TabletOS/AutoTf.TabletOS.Avalonia");
 		
-		Dispatcher.UIThread.Invoke(() =>
+		await Dispatcher.UIThread.InvokeAsync(() =>
 		{
 			UpdateText.IsVisible = true;
 			InfoStatus.IsVisible = true;
 			InfoOutput.IsVisible = true;
+		});
+		await Task.Delay(50);
 
-			string evalOutput = Statics.ExecuteCommand("eval $(\"ssh-agent\")");
-			Console.WriteLine(evalOutput);
-			InfoOutput.Text = evalOutput;
+		string evalOutput = Statics.ExecuteCommand("eval $(\"ssh-agent\")");
+		Console.WriteLine(evalOutput);
+		
+		await Dispatcher.UIThread.InvokeAsync(() =>InfoOutput.Text = evalOutput);
+		await Task.Delay(50);
 
-			string add = Statics.ExecuteCommand("ssh-add /home/display/githubKey");
-			Console.WriteLine(add);
-			InfoOutput.Text = add;
-			
-			string aasddd = Statics.ExecuteCommand("git reset --hard");
-			Console.WriteLine(aasddd);
+		string add = Statics.ExecuteCommand("ssh-add /home/display/githubKey");
+		Console.WriteLine(add);
+		await Dispatcher.UIThread.InvokeAsync(() => InfoOutput.Text = add);
+		await Task.Delay(50);
+		
+		string aasddd = Statics.ExecuteCommand("git reset --hard");
+		Console.WriteLine(aasddd);
+		
+		await Dispatcher.UIThread.InvokeAsync(() =>
+		{
 			InfoOutput.Text = aasddd;
-			
 			InfoStatus.Text = "Pulling";
-			string pull = Statics.ExecuteCommand("git pull");
-			Console.WriteLine(pull);
+		});
+		await Task.Delay(50);
+		
+		string pull = Statics.ExecuteCommand("git pull");
+		Console.WriteLine(pull);
+		
+		await Dispatcher.UIThread.InvokeAsync(() =>
+		{
 			InfoOutput.Text = pull;
-			
 			InfoStatus.Text = "Building";
-			string build = Statics.ExecuteCommand("dotnet build -c RELEASE -m");
-			Console.WriteLine(build);
+		});
+		await Task.Delay(50);
+		
+		string build = Statics.ExecuteCommand("dotnet build -c RELEASE -m");
+		Console.WriteLine(build);
+		
+		await Dispatcher.UIThread.InvokeAsync(() =>
+		{
 			InfoOutput.Text = build;
-			
 			InfoStatus.Text = "Perms";
-			string perms = Statics.ExecuteCommand("chmod +x /home/display/AutoTf.TabletOS/AutoTf.TabletOS/scripts/startup.sh");
-			Console.WriteLine(perms);
+		});
+		await Task.Delay(50);
+		
+		string perms = Statics.ExecuteCommand("chmod +x /home/display/AutoTf.TabletOS/AutoTf.TabletOS/scripts/startup.sh");
+		Console.WriteLine(perms);
+		
+		await Dispatcher.UIThread.InvokeAsync(() =>
+		{
 			InfoOutput.Text = perms;
-			
+		
 			InfoStatus.Text = "Reboot";
 			InfoOutput.Text = Statics.ExecuteCommand("reboot now");
 		});
+		await Task.Delay(50);
 		
 		Directory.SetCurrentDirectory(prevDir);
 	}
