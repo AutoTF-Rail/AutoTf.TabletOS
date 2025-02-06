1f2741665ff9fea00f589a452e8a7e53b4280500
chiouya <chiouyazo@gmail.com>
2025-02-06 11:27:14 +0100

    Manual video test

--- AutoTf.TabletOS.Avalonia/Views/TrainGalleryView.axaml.cs
+++ AutoTf.TabletOS.Avalonia/Views/TrainGalleryView.axaml.cs
@@ -3,8 +3,11 @@ using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
-using LibVLCSharp.Shared;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace AutoTf.TabletOS.Avalonia.Views;
 
@@ -12,6 +15,11 @@ public partial class TrainGalleryView : UserControl
 {
 	private TaskCompletionSource _taskCompletionSource;
 	private Grid _parent;
+	
+	private WriteableBitmap _videoBitmap;
+	private VideoCapture _videoCapture;
+	private DispatcherTimer _frameTimer;
+	private int _frameIndex;
 	
 	public TrainGalleryView()
 	{
@@ -21,8 +29,58 @@ public partial class TrainGalleryView : UserControl
 
 	private void Initialize()
 	{
-		MediaView.MediaPlayer.Play(new Media(new LibVLC(),
-			new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")));
+		_videoBitmap = new WriteableBitmap(new PixelSize(640, 480), new Vector(96, 96), PixelFormat.Bgra8888, AlphaFormat.Premul);
+		CameraViewBig.Source = _videoBitmap;
+
+		_videoCapture = new VideoCapture("/Users/chiouya/Downloads/output-2025-02-05_20/35/35.mp4");
+        
+		_frameIndex = 0;
+		_frameTimer = new DispatcherTimer
+		{
+			Interval = TimeSpan.FromMilliseconds(1000.0 / 30) 
+		};
+		_frameTimer.Tick += OnFrameUpdate;
+		_frameTimer.Start();
+	}
+	
+	private async void OnFrameUpdate(object? sender, EventArgs e)
+	{
+		if (!_videoCapture.IsOpened)
+			return;
+
+		Mat frame = _videoCapture.QueryFrame();
+
+		if (frame != null && !frame.IsEmpty)
+		{
+			using (var bitmap = frame.ToImage<Bgra, byte>()) 
+			{
+				UpdateFrame(bitmap);
+			}
+		}
+		else
+		{
+			_frameTimer.Stop();
+		}
+	}
+	
+	private void UpdateFrame(Image<Bgra, byte> frame)
+	{
+		byte[] frameData = frame.Bytes;
+
+		using (ILockedFramebuffer buf = _videoBitmap.Lock())
+		{
+			if (frameData.Length == buf.Size.Height * buf.Size.Width * 4)
+			{
+				unsafe
+				{
+					byte* ptr = (byte*)buf.Address;
+					for (int i = 0; i < frameData.Length; i++)
+					{
+						ptr[i] = frameData[i];
+					}
+				}
+			}
+		}
 	}
 
 	public Task Show(Grid parent)
