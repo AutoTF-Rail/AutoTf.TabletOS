6a85c02243b90bb1e9884cbe2719a2bc4320409e
chiouya <chiouyazo@gmail.com>
2025-05-30 09:18:19 +0200

    updated aic timeouts

--- AutoTf.TabletOS.Services/AicService.cs
+++ AutoTf.TabletOS.Services/AicService.cs
@@ -8,10 +8,10 @@ public class AicService : IAicService
 {
     private const string AicBasePath = "http://192.168.0.1/aic";
 
-    // The timeout is longer here because the timespan on the centralbridge is also 5 seconds, so it immediately kills itself
-    public async Task<Result<bool?>> IsAvailable() => await HttpHelper.SendGet<bool?>(AicBasePath + "/isAvailable", 7);
-    public async Task<Result> IsOnline() => await HttpHelper.SendGetRaw(AicBasePath + "/", 7);
-    public async Task<Result<string>> Version() => await HttpHelper.SendGet(AicBasePath + "/version", 7);
+    // The timeout for those requests on the CB is set to 3 seconds, so in total it is 6 seconds it could take MAX
+    public async Task<Result<bool?>> IsAvailable() => await HttpHelper.SendGet<bool?>(AicBasePath + "/isAvailable", 6);
+    public async Task<Result> IsOnline() => await HttpHelper.SendGetRaw(AicBasePath + "/", 6);
+    public async Task<Result<string>> Version() => await HttpHelper.SendGet(AicBasePath + "/version", 6);
     public async Task<Result<string[]>> LogDates() => await HttpHelper.SendGet<string[]>(AicBasePath + "/information/logDates", 7);
     public async Task<Result<string[]>> Logs(string date) => await HttpHelper.SendGet<string[]>(AicBasePath + $"/information/logs?date={date}", 7);
     
